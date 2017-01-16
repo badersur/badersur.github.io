07f06a4feea68eb7688483e772d7288c395e480c
Bader Nasser <badersur@gmail.com>
2017-01-16 14:47:23 +0400

    Rename nunjucks' filters

--- js/nunjucks_config.js
+++ js/nunjucks_config.js
@@ -1,8 +1,8 @@
 var env = nunjucks.configure('/views', { autoescape: true });
-env.addFilter('pretty_date', function (dateString) {
+env.addFilter('relative_time', function (dateString) {
     return moment(dateString).fromNow();
 });
-env.addFilter('friendly_date', function (dateString) {
+env.addFilter('friendly_time', function (dateString) {
     // "Sunday, February 14th 2010, 3:25:50 pm"
     return moment(dateString).format("dddd, MMMM Do YYYY, h:mm:ss a");
 });
