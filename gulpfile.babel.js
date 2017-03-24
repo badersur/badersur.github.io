601528c1141b4d01ded04f8b73cf86a0009a912a
Bader Nasser <badersur@gmail.com>
2017-03-24 15:58:00 +0400

    Register Service Workers based on Lighthouse's Chrome extension

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -69,7 +69,7 @@ gulp.task('copy', () => {
   }).pipe(gulp.dest('dist'))
     .pipe($.size({title: 'copy'}));
 
-  gulp.src(['app/data/{courses,projects}.json'])
+  gulp.src(['app/data/*'])
     .pipe(gulp.dest('dist/data'))
     .pipe($.size({title: 'copy data'}));
 }
@@ -114,19 +114,15 @@ gulp.task('styles', () => {
 // `.babelrc` file.
 gulp.task('scripts', () =>
     gulp.src([
-      // Note: Since we are not using useref in the scripts build pipeline,
-      //       you need to explicitly list your scripts here in the right order
-      //       to be correctly concatenated
-      './app/scripts/main.js'
-      // Other scripts
+      './app/scripts/*.js'
     ])
       .pipe($.newer('.tmp/scripts'))
       .pipe($.sourcemaps.init())
       .pipe($.babel())
       .pipe($.sourcemaps.write())
       .pipe(gulp.dest('.tmp/scripts'))
-      .pipe($.concat('main.min.js'))
       .pipe($.uglify({preserveComments: 'some'}))
+      .pipe($.rename({suffix: '.min'}))
       // Output files
       .pipe($.size({title: 'scripts'}))
       .pipe($.sourcemaps.write('.'))
