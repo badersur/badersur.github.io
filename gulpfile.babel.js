175c230807a418877591a905e44d5135c8b986bb
Bader Nasser <badersur@gmail.com>
2017-11-12 16:45:20 +0400

    Switch to gulp 4 & node 9

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,23 +1,25 @@
 'use strict';
 
-// This gulpfile makes use of new JavaScript features.
-// Babel handles this without us having to do anything. It just works.
 // You can read more about the new JavaScript features here:
 // https://babeljs.io/docs/learn-es2015/
 
 import path from 'path';
 import gulp from 'gulp';
 import del from 'del';
-import runSequence from 'run-sequence';
 import browserSync from 'browser-sync';
 import swPrecache from 'sw-precache';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import moduleImporter from 'sass-module-importer';
 import MarkdownIt from 'markdown-it';
-import { readFileSync as fsRead } from 'fs';
-import { safeLoad as yamlSafeLoad } from 'js-yaml';
-
-import { stream as critical } from 'critical';
+import {
+  readFileSync as fsRead
+} from 'fs';
+import {
+  safeLoad as yamlSafeLoad
+} from 'js-yaml';
+import {
+  stream as critical
+} from 'critical';
 
 import pkg from './package.json';
 
@@ -48,50 +50,59 @@ const manageEnvironment = environment => {
 
 console.log(`\nBuilding for ${finalDestination}...\n`);
 
-// Lint JavaScript
-gulp.task('lint', () =>
-  gulp.src(['app/scripts/**/*.js', '!node_modules/**'])
-    .pipe($.eslint())
-    .pipe($.eslint.format())
-    .pipe($.if(!browserSync.active, $.eslint.failAfterError()))
-);
-
-// Optimize images
-gulp.task('images', () =>
-  gulp.src('app/images/**/*{png,svg}')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
-    .pipe(gulp.dest(`${finalDestination}/images`))
-    .pipe($.size({ title: 'images' }))
-);
+// Clean output directory
+gulp.task('clean', () => del([
+  '.tmp',
+  `${finalDestination}/*`,
+  `!${finalDestination}/.git`
+], {
+  dot: true
+}));
 
 // Copy files
 gulp.task('copy', () => {
   gulp.src([
-    'app/*',
-    '!app/*.html',
-    '!app/robots.txt',
-    '!app/sitemap.xml',
-    '!app/templates'
-  ])
+      './app/*',
+      '!./app/*.html',
+      '!./app/robots.txt',
+      '!./app/sitemap.xml',
+      '!./app/templates'
+    ])
     .pipe(gulp.dest(finalDestination))
-    .pipe($.size({ title: 'copy root' }));
+    .pipe($.size({
+      title: 'copy root'
+    }));
 
-  gulp.src(['app/data/*'])
+  gulp.src('./app/data/*')
     .pipe(gulp.dest(`${finalDestination}/data`))
-    .pipe($.size({ title: 'copy data' }));
+    .pipe($.size({
+      title: 'copy data'
+    }));
 
-  gulp.src([
-    'node_modules/nunjucks/browser/nunjucks.min.js',
-    'node_modules/jquery/dist/jquery.min.js'
-  ])
+  return gulp.src([
+      './node_modules/nunjucks/browser/nunjucks.min.js',
+      './node_modules/jquery/dist/jquery.min.js'
+    ])
     .pipe($.concat('vendors.js'))
     .pipe(gulp.dest(`${finalDestination}/scripts`))
     .pipe(gulp.dest('.tmp/scripts'))
-    .pipe($.size({ title: 'copy & concat 3rd party scripts' }));
+    .pipe($.size({
+      title: 'copy & concat 3rd party scripts'
+    }));
 });
+
+// Optimize images
+gulp.task('images', () =>
+  gulp.src('./app/images/**/*{png,svg}')
+  .pipe($.cache($.imagemin({
+    progressive: true,
+    interlaced: true
+  })))
+  .pipe(gulp.dest(`${finalDestination}/images`))
+  .pipe($.size({
+    title: 'images'
+  }))
+);
 
 // Compile and automatically prefix stylesheets
 gulp.task('styles', () => {
@@ -108,9 +119,7 @@ gulp.task('styles', () => {
   ];
 
   // For best performance, don't add Sass partials to `gulp.src`
-  return gulp.src([
-    'app/styles/main.scss'
-  ])
+  return gulp.src('./app/styles/main.scss')
     .pipe($.newer('.tmp/styles'))
     .pipe($.sourcemaps.init())
     .pipe($.sass({
@@ -121,52 +130,47 @@ gulp.task('styles', () => {
     .pipe(gulp.dest('.tmp/styles'))
     // Concatenate and minify styles
     .pipe($.if('*.css', $.cssnano()))
-    .pipe($.size({ title: 'styles' }))
+    .pipe($.size({
+      title: 'styles'
+    }))
     .pipe($.sourcemaps.write('./'))
     .pipe(gulp.dest(`${finalDestination}/styles`))
     .pipe(gulp.dest('.tmp/styles'));
 });
 
-// Generate & Inline Critical-path CSS
-gulp.task('critical', () => {
-  return gulp.src([
-    `${finalDestination}/**/*.html`,
-    `!${finalDestination}/index.html`,
-    `!${finalDestination}/courses.html`,
-    `!${finalDestination}/projects.html`
+// Lint JavaScript
+gulp.task('lint', () =>
+  gulp.src([
+    './app/scripts/**/*.js',
+    '!node_modules/**'
   ])
-    .pipe(critical({
-      base: `${finalDestination}/`,
-      inline: true,
-      minify: true,
-      css: [`${finalDestination}/styles/main.css`],
-      // 5 minutes
-      timeout: 5 * 60 * 1000
-    }))
-    .on('error', err => console.log(err.message))
-    .pipe(gulp.dest(finalDestination));
-});
+  .pipe($.eslint())
+  .pipe($.eslint.format())
+  .pipe($.if(!browserSync.active, $.eslint.failAfterError()))
+);
 
 // Concatenate and minify JavaScript. Also, transpile ES2015 code to ES5.
-gulp.task('scripts', () => {
-  return gulp.src([
+gulp.task('scripts', () =>
+  gulp.src([
     './node_modules/material-design-lite/src/mdlComponentHandler.js',
     './node_modules/material-design-lite/src/layout/layout.js',
     './app/scripts/main.js'
   ])
-    .pipe($.newer('.tmp/scripts'))
-    .pipe($.sourcemaps.init())
-    .pipe($.babel())
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('.tmp/scripts'))
-    .pipe($.concat('main.js'))
-    .pipe($.uglify())
-    // Output files
-    .pipe($.size({ title: 'main script' }))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest(`${finalDestination}/scripts`))
-    .pipe(gulp.dest('.tmp/scripts'));
-});
+  .pipe($.newer('.tmp/scripts'))
+  .pipe($.sourcemaps.init())
+  .pipe($.babel())
+  .pipe($.sourcemaps.write())
+  .pipe(gulp.dest('.tmp/scripts'))
+  .pipe($.concat('main.js'))
+  .pipe($.uglify())
+  // Output files
+  .pipe($.size({
+    title: 'main script'
+  }))
+  .pipe($.sourcemaps.write('.'))
+  .pipe(gulp.dest(`${finalDestination}/scripts`))
+  .pipe(gulp.dest('.tmp/scripts'))
+);
 
 // Scan your HTML for assets & optimize them
 gulp.task('html', () => {
@@ -180,13 +184,16 @@ gulp.task('html', () => {
     console.log(e);
   }
 
-  return gulp.src(['app/**/*.html', '!app/templates/*'])
+  return gulp.src([
+      './app/**/*.html',
+      '!./app/templates/*'
+    ])
     .pipe($.nunjucksRender({
       envOptions: {
         autoescape: false
       },
       manageEnv: manageEnvironment,
-      path: 'app/templates/',
+      path: './app/templates/',
       data: {
         isGAE,
         baseUrl,
@@ -218,68 +225,96 @@ gulp.task('html', () => {
       removeStyleLinkTypeAttributes: true
     })))
     // Output files
-    .pipe($.if('*.html', $.size({ title: 'html', showFiles: true })))
+    .pipe($.if('*.html', $.size({
+      title: 'html',
+      showFiles: true
+    })))
     .pipe(gulp.dest(finalDestination));
 });
 
 // Build the sitemap & update robots.txt
-gulp.task('sitemap', () => {
-
-  return gulp.src(['app/sitemap.xml', 'app/robots.txt'])
-    .pipe($.nunjucksRender({
-      inheritExtension: true,
-      envOptions: {
-        autoescape: false
-      },
-      manageEnv: manageEnvironment,
-      data: {
-        paths,
-        baseUrl,
-        languages
-      }
-    }))
-    .pipe(gulp.dest(finalDestination));
-});
+gulp.task('sitemap', () =>
+  gulp.src([
+    './app/sitemap.xml',
+    './app/robots.txt'
+  ])
+  .pipe($.nunjucksRender({
+    inheritExtension: true,
+    envOptions: {
+      autoescape: false
+    },
+    manageEnv: manageEnvironment,
+    data: {
+      paths,
+      baseUrl,
+      languages
+    }
+  }))
+  .pipe(gulp.dest(finalDestination))
+);
 
 // Watch files for changes & reload
-gulp.task('serve', ['html', 'scripts', 'styles', 'copy'], () => {
-  browserSync({
-    notify: false,
-    // Customize the Browsersync console logging prefix
-    logPrefix: 'WSK',
-    // Allow scroll syncing across breakpoints
-    scrollElementMapping: ['main', '.mdl-layout'],
-    // Run as an https by uncommenting 'https: true'
-    // Note: this uses an unsigned certificate which on first access
-    //       will present a certificate warning in the browser.
-    // https: true,
-    server: ['.tmp', 'app'],
-    port: 3000,
-    // don't open any URL automatically
-    open: false
-  });
+gulp.task('serve', gulp.series(
+  'clean',
+  gulp.parallel('html', 'scripts', 'styles', 'copy', 'sitemap'),
+  () => {
+    browserSync({
+      notify: false,
+      // Customize the Browsersync console logging prefix
+      logPrefix: 'WSK',
+      // Allow scroll syncing across breakpoints
+      scrollElementMapping: ['main', '.mdl-layout'],
+      // Run as an https by uncommenting 'https: true'
+      // Note: this uses an unsigned certificate which on first access
+      //       will present a certificate warning in the browser.
+      // https: true,
+      server: ['.tmp', 'app'],
+      port: 3000,
+      // don't open any URL automatically
+      open: false
+    });
 
-  gulp.watch([
-    'app/**/*.{html,yaml,json}',
-  ], ['html', reload]);
-  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
-  gulp.watch(['app/scripts/**/*.js'], ['lint', 'scripts', reload]);
-  gulp.watch(['app/images/**/*'], reload);
-});
+    gulp.watch('./app/**/*.{html,yaml,json}', gulp.series('html', reload));
+    gulp.watch('./app/styles/**/*.{scss,css}', gulp.series('styles', reload));
+    gulp.watch('./app/scripts/**/*.js', gulp.series('lint', 'scripts', reload));
+    gulp.watch('./app/*.{xml,txt}', gulp.series('sitemap'));
+    gulp.watch('./app/images/**/*', reload);
+  }
+));
+
+// Generate & Inline Critical-path CSS
+gulp.task('critical', () =>
+  gulp.src([
+    `${finalDestination}/**/*.html`,
+    `!${finalDestination}/index.html`,
+    `!${finalDestination}/courses.html`,
+    `!${finalDestination}/projects.html`
+  ])
+  .pipe(critical({
+    base: `${finalDestination}/`,
+    inline: true,
+    minify: true,
+    css: [`${finalDestination}/styles/main.css`],
+    // 5 minutes
+    timeout: 5 * 60 * 1000
+  }))
+  .on('error', err => console.log(err.message))
+  .pipe(gulp.dest(finalDestination))
+);
 
 // Copy over the scripts that are used in importScripts as part of the
 // generate-service-worker task.
-gulp.task('copy-sw-scripts', () => {
-  return gulp.src([
-    'node_modules/sw-toolbox/sw-toolbox.js',
-    'app/scripts/sw/runtime-caching.js'
+gulp.task('copy-sw-scripts', () =>
+  gulp.src([
+    './node_modules/sw-toolbox/sw-toolbox.js',
+    './app/scripts/sw/runtime-caching.js'
   ])
-    .pipe(gulp.dest(`${finalDestination}/scripts/sw`));
-});
+  .pipe(gulp.dest(`${finalDestination}/scripts/sw`))
+);
 
 // Append content hash to filenames
-gulp.task('revision', ['copy-sw-scripts'], () => {
-  return gulp.src([
+gulp.task('revision', () =>
+  gulp.src([
     `${finalDestination}/**/*.css`,
     `${finalDestination}/**/*.js`,
     `!${finalDestination}/sw.js`,
@@ -287,15 +322,15 @@ gulp.task('revision', ['copy-sw-scripts'], () => {
     `${finalDestination}/**/*.svg`,
     `${finalDestination}/**/*.ico`
   ])
-    .pipe($.rev())
-    .pipe($.revFormat({
-      prefix: '.'
-    }))
-    .pipe($.revDeleteOriginal())
-    .pipe(gulp.dest(finalDestination))
-    .pipe($.rev.manifest())
-    .pipe(gulp.dest(finalDestination));
-});
+  .pipe($.rev())
+  .pipe($.revFormat({
+    prefix: '.'
+  }))
+  .pipe($.revDeleteOriginal())
+  .pipe(gulp.dest(finalDestination))
+  .pipe($.rev.manifest())
+  .pipe(gulp.dest(finalDestination))
+);
 
 // See http://www.html5rocks.com/en/tutorials/service-worker/introduction/ for
 // an in-depth explanation of what service workers are and why you should care.
@@ -345,42 +380,20 @@ gulp.task('generate-service-worker', () => {
 });
 
 // Minify service worker.
-gulp.task('minify-service-worker', () => {
-  return gulp.src([
-    `./${finalDestination}/sw.js`
-  ])
-    .pipe($.uglify())
-    .pipe(gulp.dest(finalDestination));
-});
-
-// Clean output directory
-gulp.task('clean', () => del([
-  '.tmp',
-  `${finalDestination}/*`,
-  `!${finalDestination}/.git`
-], { dot: true }));
-
-// Build production files
-gulp.task('build', ['clean'], cb =>
-  runSequence(
-    'styles',
-    ['lint', 'html', 'scripts', 'images', 'copy', 'sitemap'],
-    'critical',
-    'revision',
-    'generate-service-worker',
-    'minify-service-worker',
-    cb
-  )
+gulp.task('minify-service-worker', () =>
+  gulp.src(`./${finalDestination}/sw.js`)
+  .pipe($.uglify())
+  .pipe(gulp.dest(finalDestination))
 );
 
 // Rewrite occurences of filenames which have been renamed by gulp-rev
-gulp.task('replace', ['build'], () => {
+gulp.task('replace', () => {
   const manifest = gulp.src(`./${finalDestination}/rev-manifest.json`);
 
   return gulp.src([
-    `${finalDestination}/**/*.html`,
-    `${finalDestination}/manifest.*`
-  ])
+      `${finalDestination}/**/*.html`,
+      `${finalDestination}/manifest.*`
+    ])
     .pipe($.revReplace({
       manifest: manifest,
       replaceInExtensions: ['.html', '.webmanifest', '.webapp']
@@ -388,10 +401,20 @@ gulp.task('replace', ['build'], () => {
     .pipe(gulp.dest(finalDestination));
 });
 
-gulp.task('default', ['replace']);
+// Build production files
+gulp.task('default', gulp.series(
+  'clean',
+  gulp.parallel('lint', 'html', 'styles', 'scripts', 'images', 'copy', 'sitemap'),
+  'critical',
+  'copy-sw-scripts',
+  'revision',
+  'generate-service-worker',
+  'minify-service-worker',
+  'replace'
+));
 
 // Build and serve the output from the dist build
-gulp.task('serve:dist', ['default'], () =>
+gulp.task('serve:dist', gulp.series('default', () =>
   browserSync({
     notify: false,
     logPrefix: 'WSK',
@@ -406,8 +429,4 @@ gulp.task('serve:dist', ['default'], () =>
     // don't open any URL automatically
     open: false
   })
-);
-
-// Load custom tasks from the `tasks` directory
-// Run: `npm install --save-dev require-dir` from the command-line
-// try { require('require-dir')('tasks'); } catch (err) { console.error(err); }
+));
