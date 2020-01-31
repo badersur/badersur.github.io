12405f6d31a329154d4c6fcb57f3c7f7349a3f64
Bader Nasser <badersur@gmail.com>
2020-01-31 21:38:17 +0400

    Use TypeScript for Gatsby's API Files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,80 +1,11 @@
-/* eslint-disable @typescript-eslint/camelcase */
+'use strict';
 
-const PORT = process.env.NODE_ENV === 'development' ? 8000 : 9000;
-const siteUrl = process.env.siteUrl || `http://localhost:${PORT}`;
-const trackingId = process.env.trackingId || '';
+/**
+ * Run TypeScript code without compiling it
+ * ts-node register helps compiling and importing TypeScript modules
+ * credit: https://github.com/assainov/gatsby-extensive-starter-typescript/blob/master/gatsby-config.js
+ */
+require('ts-node').register();
 
-module.exports = {
-    siteMetadata: {
-        siteUrl,
-        twitter: '@BaderSur',
-        title: {
-            ar: 'بدر العفية',
-            en: 'BaderSur',
-        },
-    },
-    plugins: [
-        'gatsby-plugin-react-helmet',
-        'gatsby-transformer-yaml',
-        'gatsby-plugin-catch-links',
-        {
-            resolve: 'gatsby-plugin-typescript',
-            options: {
-                isTSX: true,
-                allExtensions: true,
-                // jsxPragma: 'jsx', // defaults to "React"
-            },
-        },
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                path: `${__dirname}/src/data`,
-            },
-        },
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                name: 'images',
-                path: `${__dirname}/src/images`,
-            },
-        },
-        'gatsby-transformer-sharp',
-        'gatsby-plugin-sharp',
-        {
-            resolve: 'gatsby-plugin-sitemap',
-            options: {
-                exclude: ['/index.html', '/en/404.html', '/ar/404.html'],
-            },
-        },
-        {
-            resolve: 'gatsby-plugin-google-analytics',
-            options: {
-                trackingId,
-                head: false,
-                anonymize: true,
-                respectDNT: true,
-                siteSpeedSampleRate: 10,
-            },
-        },
-        {
-            resolve: 'gatsby-plugin-manifest',
-            options: {
-                name: 'BaderSur',
-                short_name: 'BaderSur',
-                start_url: '/en/?source=pwa&utm_source=a2hs',
-                background_color: '#663399',
-                theme_color: '#663399',
-                // In order to show the Add to Home Screen Prompt,
-                // display must be set to standalone.
-                display: 'standalone',
-                // This path is relative to the root of the site.
-                icon: 'src/images/logo.png',
-                // `query`(default), `name`, or `none`
-                cache_busting_mode: 'name',
-            },
-        },
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        'gatsby-plugin-offline',
-    ],
-};
+// @ts-ignore
+module.exports = require('./gatsby-config.ts');
---
+++ gatsby-node.ts
@@ -0,0 +1,74 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+import path from 'path';
+import { GatsbyNode } from 'gatsby';
+
+const supportedLangs = ['en', 'ar'];
+const defaultLang = 'en';
+
+const createPages: GatsbyNode['createPages'] = ({ actions }) => {
+    const { createPage, createRedirect } = actions;
+
+    const indexTemplate = path.resolve('./src/templates/index.tsx');
+    const projectsTemplate = path.resolve('./src/templates/projects.tsx');
+    const coursesTemplate = path.resolve('./src/templates/courses.tsx');
+    const errorTemplate = path.resolve('./src/templates/404.tsx');
+
+    supportedLangs.forEach(lang => {
+        createPage({
+            path: `/${lang}/`,
+            component: indexTemplate,
+            context: { lang },
+        });
+
+        createPage({
+            path: `/${lang}/projects/`,
+            component: projectsTemplate,
+            context: { lang },
+        });
+
+        createPage({
+            path: `/${lang}/404.html`,
+            component: errorTemplate,
+            context: { lang },
+        });
+
+        if (lang === defaultLang) {
+            createPage({
+                path: '/index.html',
+                component: indexTemplate,
+                context: { lang },
+            });
+
+            createPage({
+                path: `/${lang}/courses/`,
+                component: coursesTemplate,
+                context: {},
+            });
+
+            createPage({
+                path: '/404.html',
+                component: errorTemplate,
+                context: { lang, multiLangs: true },
+            });
+        }
+    });
+
+    createRedirect({
+        fromPath: '/',
+        toPath: `/${defaultLang}/`,
+        redirectInBrowser: true,
+    });
+
+    createRedirect({
+        fromPath: '/index.html',
+        toPath: `/${defaultLang}/`,
+        redirectInBrowser: true,
+    });
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { createPages };
